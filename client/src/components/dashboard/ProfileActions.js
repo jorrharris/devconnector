a4c8e44803d2211249b4bd06c45a79cb2004f0fa
MSI\jorrh <jordanharris@cpp.edu>
2018-12-27 11:08:27 -0800

    responsive

--- client/src/components/dashboard/ProfileActions.js
+++ client/src/components/dashboard/ProfileActions.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const ProfileActions = () => {
   return (
-    <div className="btn-group mb-4" role="group">
+    <div className="btn-group mb-4 forPhone" role="group">
       <Link to="/edit-profile" className="btn btn-light">
         <i className="fa fa-user-circle text-info mr-1" /> Edit Profile
       </Link>
